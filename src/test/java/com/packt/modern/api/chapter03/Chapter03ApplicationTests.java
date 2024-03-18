a71b5c773a4ef039428b5a4df903c22aa25e9c73
mehdizebhi <mehdizebhi@yahoo.com>
2024-03-18 10:59:29 +0330

    add / create spring boot 3.2.3 project from start.spring.io

---
+++ src/test/java/com/packt/modern/api/chapter03/Chapter03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packt.modern.api.chapter03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
